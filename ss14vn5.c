8c99163cfa059b4a97420d7ba275a316aa3f8bc4
vinh15cm <147390597+vinh15cm@users.noreply.github.com>
2023-12-01 13:05:21 +0700

    Add files via upload

---
+++ ss14vn5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int numbers[100]={5,4,3,2,1} ;
+	int *ptr= numbers;
+	printf("day so tang dan:\n");
+	for(int i=0;i<5;i++){
+		for(int j=i+1;j<5;j++){
+			if(*(ptr+i)>*(ptr+j)){
+				int temp = *(ptr+i);
+				*(ptr+i)=*(ptr+j);
+				*(ptr+j)=temp; 
+					
+				
+			}
+		} 
+	} 
+	printf("\n");
+	printf("day so giam dan :\n");
+	for(int i=0;i<5;i++){
+		if(*(ptr+i)) 
+	} 
+} 
